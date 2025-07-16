77d3492573513abb9217b4f6c5110f9c36e35aca
ereizas <ereizas@gmail.com>
2025-07-16 06:51:51 -0400

    Create divide function

--- calculator.js
+++ calculator.js
@@ -9,3 +9,10 @@ function subtract(num1,num2){
 function multiply(num1,num2){
     return +num1 * +num2;
 }
+
+function divide(num1,num2){
+    if(+num2==0){
+        return "Divide by 0 error";
+    }
+    return +num1 / +num2;
+}
