a31eb8833037e609d73d73b599f12aee88e27db2
ereizas <ereizas@gmail.com>
2025-07-17 06:27:59 -0400

    Operate on calculator inputs

--- calculator.js
+++ calculator.js
@@ -20,3 +20,18 @@ function divide(num1,num2){
     }
     return +num1 / +num2;
 }
+
+function operate(num1,operator,num2){
+    switch(operator){
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        case "/":
+            return divide(num1,num2);
+        default:
+            return "Invalid operator";
+    }
+}
